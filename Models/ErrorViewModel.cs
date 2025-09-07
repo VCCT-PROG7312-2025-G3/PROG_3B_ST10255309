521adf4e072a1daf33fd3274e5e0720ef26d431b
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2025-09-07 12:35:10 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG_3B_ST10255309.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
