603da57e2ab7510155dbdbb97e4aa13ef9a4f291
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2025-09-08 08:54:41 +0200

    Initial Commit

---
+++ Controllers/ReportIssuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG_3B_ST10255309.Controllers
+{
+    public class ReportIssuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG_3B_ST10255309.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
